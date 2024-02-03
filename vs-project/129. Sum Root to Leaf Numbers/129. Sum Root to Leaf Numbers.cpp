94bf8d4eddc908febf46a6a57867146fff12ba11
mardyu <michealyxd@hotmail.com>
2024-02-03 21:32:30 +0800

    - add some leet code answer

---
+++ vs-project/129. Sum Root to Leaf Numbers/129. Sum Root to Leaf Numbers.cpp
@@ -0,0 +1,72 @@
+// 129. Sum Root to Leaf Numbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+    void doCalc(const TreeNode* node, int prev, int& sum) {
+        if (nullptr == node)
+            return;
+
+        if (node->left == nullptr && node->right == nullptr) {
+            sum += prev * 10 + node->val;
+        }
+
+        prev = prev * 10 + node->val;
+
+        if (node->left != nullptr) {
+            doCalc(node->left, prev, sum);
+        }
+        if (node->right != nullptr) {
+            doCalc(node->right, prev, sum);
+        }
+    }
+
+public:
+    int sumNumbers(TreeNode* root) {
+
+        int sum = 0;
+
+        doCalc(root, 0, sum);
+
+        return sum;
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
